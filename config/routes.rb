ccc88cc8ea6621d0926751d3f2b89d151f647766
EC2 Default User <gakuto404@gmail.com>
2022-03-31 16:24:02 +0000

    やり直しました

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  root to: "homes#top"
+  devise_for :users
+  get 'homes/top' => 'homes#top' ,as: :top
+  get 'homes/about' => 'homes#about' ,as: :about
+  resources :users, only: [:show, :edit, :index]
+  resources :books, only: [:new, :index, :show, :edit]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
