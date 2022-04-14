20aec0de4ee364587c7a83b751adbdf2bad08011
EC2 Default User <gakuto404@gmail.com>
2022-04-14 13:09:55 +0000

    RSpec完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   devise_for :users
-  get 'homes/top' => 'homes#top' ,as: :top
-  get 'homes/about' => 'homes#about' ,as: :about
-  post 'books' => 'books#create'
-  resources :users, only: [:show, :edit, :index]
-  resources :books, only: [:create, :new, :index, :show, :edit, :destroy]
-  patch 'users/:id' => 'users#update', as: 'update_user'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  get 'home/about' => 'homes#about' ,as: :about
+  resources :users, only: [:show, :edit, :index, :update]
+  resources :books, only: [:create, :new, :index, :show, :edit, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
